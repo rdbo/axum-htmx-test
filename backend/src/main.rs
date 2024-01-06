255a841e362f18213ef949c3d0c8e293884e6603
rdbo <rdbodev@gmail.com>
2024-01-06 18:39:42 -0300

    added sqlx database connection

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,9 +3,10 @@ use axum::{
     http::HeaderMap,
     response::{Html, IntoResponse},
     routing::{get, post},
-    Form, Router,
+    Extension, Form, Router,
 };
 use serde::Deserialize;
+use sqlx::postgres::{PgPool, PgPoolOptions};
 use tower_http::services::ServeDir;
 
 #[derive(Template)]
@@ -14,9 +15,17 @@ struct IndexTemplate<'a> {
     name: &'a str,
 }
 
+#[derive(sqlx::FromRow, Deserialize, Debug)]
+struct User {
+    id: i32,
+    name: String,
+}
+
 #[derive(Template)]
 #[template(path = "mypage.html")]
-struct MyPageTemplate {}
+struct MyPageTemplate {
+    users: Vec<User>,
+}
 
 async fn index() -> Html<String> {
     let template = IndexTemplate { name: "World" };
@@ -27,8 +36,12 @@ async fn click() -> Html<&'static str> {
     Html("<h2>You clicked the button</h2>")
 }
 
-async fn mypage() -> Html<String> {
-    let template = MyPageTemplate {};
+async fn mypage(Extension(dbpool): Extension<PgPool>) -> Html<String> {
+    let users = sqlx::query_as::<_, User>("SELECT * FROM user_account")
+        .fetch_all(&dbpool)
+        .await
+        .unwrap();
+    let template = MyPageTemplate { users };
     Html(template.render().unwrap())
 }
 
@@ -50,12 +63,21 @@ async fn rename(Form(form): Form<Rename>) -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
+    let dbpool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&"postgresql://postgres@localhost/test")
+        .await
+        .expect("failed to connect to database");
+
     let app = Router::new()
         .route("/", get(index))
         .route("/click", post(click))
         .route("/rename", post(rename))
         .route("/mypage", get(mypage))
+        .layer(Extension(dbpool))
         .fallback_service(ServeDir::new("static"));
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+
     axum::serve(listener, app).await.unwrap();
 }
